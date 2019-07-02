3cbb4295f35227bdba2a0746d2d31328cce226ee
CarloBerni <carlo.berni@hetic.net>
2019-07-02 12:23:24 +0200

    continue

--- little-course/src/app/app.component.ts
+++ little-course/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'little-course';
+  isAuth = false;
+
+  appareilOne = "rig Ethereum"
+  appareilTwo = "rig Litecoin";
+  appareilThree = "rig Dash";
+  appareilFour = "rig Monero";
+
+
+  constructor() {
+    setTimeout(() => {
+      this.isAuth = true;
+    }, 2500);
+  }
+  onLightOn() {
+    console.log("On allume tout !");
+  }
 }
