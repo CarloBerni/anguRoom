f08a05c58cfa8a42b691836e22f52b9ad3d102cf
CarloBerni <carlo.berni@hetic.net>
2019-07-03 10:59:47 +0200

    add routing app- appareil-view

--- little-course/src/app/app.component.ts
+++ little-course/src/app/app.component.ts
@@ -1,40 +1,10 @@
 import { Component, OnInit } from "@angular/core";
-import { AppareilService } from "./services/appareil.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent implements OnInit {
-  isAuth = false;
-  lastUpdate = new Promise((resolve, reject) => {
-    const date = new Date();
-    setTimeout(() => {
-      resolve(date);
-    }, 1500);
-  });
-
-  appareils: any[];
-
-  constructor(private appareilService: AppareilService) {}
-
-  ngOnInit() {
-    this.appareils = this.appareilService.appareils;
-  }
-
-  onAllumer() {
-    if (confirm("Etes-vous sûr de vouloir allumer tous vos appareils ?")) {
-      this.appareilService.switchOnAll();
-    } else {
-      return null;
-    }
-  }
-  onEteindre() {
-    if (confirm("Etes-vous sûr de vouloir éteindre tous vos appareils ?")) {
-      this.appareilService.switchOffAll();
-    } else {
-      return null;
-    }
-  }
+export class AppComponent {
+  constructor() {}
 }
--- little-course/src/app/app.module.ts
+++ little-course/src/app/app.module.ts
@@ -8,12 +8,16 @@ import { AppareilComponent } from './appareil/appareil.component';
 import { FormsModule } from '@angular/forms';
 
 import { AppareilService } from './services/appareil.service';
+import { AuthComponent } from './auth/auth.component';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    AppareilComponent
+    AppareilComponent,
+    AuthComponent,
+    AppareilViewComponent
   ],
   imports: [
     BrowserModule,
